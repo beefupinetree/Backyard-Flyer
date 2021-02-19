823e7b9792a9d14825ec3fbc979747ab53d1d6ec
Tarek <tsamhan@gmail.com>
2021-02-18 21:01:53 -0500

    first commit

--- backyard_flyer.py
+++ backyard_flyer.py
@@ -19,7 +19,6 @@ class States(Enum):
 
 
 class BackyardFlyer(Drone):
-
     def __init__(self, connection):
         super().__init__(connection)
         self.target_position = np.array([0.0, 0.0, 0.0])
@@ -49,7 +48,10 @@ class BackyardFlyer(Drone):
 
         This triggers when `MsgID.LOCAL_VELOCITY` is received and self.local_velocity contains new data
         """
-        pass
+        if self.flight_state == States.LANDING:
+            if self.global_position[2] - self.global_home[2] < 0.1:
+                if abs(self.local_position[2]) < 0.01:
+                    self.disarming_transition()
 
     def state_callback(self):
         """
@@ -64,16 +66,21 @@ class BackyardFlyer(Drone):
         
         1. Return waypoints to fly a box
         """
-        pass
+        square = [(10, 0, 3), (10, 10, 3), (0, 10, 3), (0, 0, 3)]
+        return square
 
     def arming_transition(self):
-        """TODO: Fill out this method
-        
+        """        
         1. Take control of the drone
         2. Pass an arming command
         3. Set the home location to current position
         4. Transition to the ARMING state
         """
+        self.take_control()
+        self.arm()
+        self.set_home_position(self.global_position)
+        self.flight_state = States.ARMING
+
         print("arming transition")
 
     def takeoff_transition(self):
@@ -83,6 +90,9 @@ class BackyardFlyer(Drone):
         2. Command a takeoff to 3.0m
         3. Transition to the TAKEOFF state
         """
+        self.target_position = [0, 0, 3]
+        self.takeoff(self.target_position[2])
+        self.flight_state = States.TAKEOFF
         print("takeoff transition")
 
     def waypoint_transition(self):
@@ -91,22 +101,26 @@ class BackyardFlyer(Drone):
         1. Command the next waypoint position
         2. Transition to WAYPOINT state
         """
+        self.cmd_position(north, east, altitude, heading)
+        self.flight_state = States.WAYPOINT
         print("waypoint transition")
 
     def landing_transition(self):
-        """TODO: Fill out this method
-        
+        """        
         1. Command the drone to land
         2. Transition to the LANDING state
         """
+        self.land()
+        self.flight_state = States.LANDING
         print("landing transition")
 
     def disarming_transition(self):
-        """TODO: Fill out this method
-        
+        """        
         1. Command the drone to disarm
         2. Transition to the DISARMING state
         """
+        self.disarm()
+        self.flight_state = States.DISARMING
         print("disarm transition")
 
     def manual_transition(self):
@@ -141,12 +155,16 @@ class BackyardFlyer(Drone):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=5760, help='Port number')
-    parser.add_argument('--host', type=str, default='127.0.0.1', help="host address, i.e. '127.0.0.1'")
+    parser.add_argument("--port", type=int, default=5760, help="Port number")
+    parser.add_argument(
+        "--host", type=str, default="127.0.0.1", help="host address, i.e. '127.0.0.1'"
+    )
     args = parser.parse_args()
 
-    conn = MavlinkConnection('tcp:{0}:{1}'.format(args.host, args.port), threaded=False, PX4=False)
-    #conn = WebSocketConnection('ws://{0}:{1}'.format(args.host, args.port))
+    conn = MavlinkConnection(
+        "tcp:{0}:{1}".format(args.host, args.port), threaded=False, PX4=False
+    )
+    # conn = WebSocketConnection('ws://{0}:{1}'.format(args.host, args.port))
     drone = BackyardFlyer(conn)
     time.sleep(2)
     drone.start()
